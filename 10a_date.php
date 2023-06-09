1eb3858c808ac89669b7dcfd72a3946f030d008f
Bhuvanchandra M <bhuvanchandra668@gmail.com>
2023-06-09 19:14:37 +0530

    Changes

---
+++ 10a_date.php
@@ -0,0 +1,12 @@
+<?php
+
+// Get the current date
+$date = new DateTime();
+
+// Format the date in dd-mm-yyyy format
+$formattedDate = $date->format('d-m-Y');
+
+// Print the formatted date
+echo $formattedDate;
+
+?>
